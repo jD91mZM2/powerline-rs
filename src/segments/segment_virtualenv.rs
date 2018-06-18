61a26688c37ed96479387762c0fbfeefffa143ab
Vlad Frolov <frolvlad@gmail.com>
2018-06-18 17:58:16 +0300

    Added Python VirtualEnv segment

---
+++ src/segments/segment_virtualenv.rs
@@ -0,0 +1,19 @@
+use std::{env, path};
+use {Powerline, Segment};
+
+pub fn segment_virtualenv(p: &mut Powerline) {
+    if let Ok(Some(virtual_env_name)) = env::var("VIRTUAL_ENV")
+        .or_else(|_| env::var("CONDA_ENV_PATH"))
+        .or_else(|_| env::var("CONDA_DEFAULT_ENV"))
+        .map(|env_path| {
+            path::Path::new(&env_path)
+                .file_name()
+                .and_then(|env_name| Some(env_name.to_string_lossy().into_owned()))
+        }) {
+        p.segments.push(Segment::new(
+            p.theme.virtual_env_bg,
+            p.theme.virtual_env_fg,
+            virtual_env_name,
+        ));
+    }
+}
