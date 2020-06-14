aaeb0a98536c1408c6f579d2afe7b9ff452d3987
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-06-14 12:17:39 +0000

    Fix different linebreak line

--- src/segments/segment_linebreak.rs
+++ src/segments/segment_linebreak.rs
@@ -4,7 +4,7 @@ use crate::{Powerline, Segment, Shell};
 pub fn segment_linebreak(p: &mut Powerline) {
     let (bg, fg) = (0, 0);
     p.segments.push(match p.shell {
-        Shell::Bare => Segment::new(bg, fg, { Cow::from(String::from("\n")) }).with_no_space_after(),
+        Shell::Bare => Segment::new(bg, fg, "\n").dont_escape().with_no_space_after(),
         Shell::Bash => Segment::new(bg, fg, "\n").dont_escape().with_no_space_after(),
         Shell::Zsh => Segment::new(bg, fg, "\n").dont_escape().with_no_space_after(),
     });
